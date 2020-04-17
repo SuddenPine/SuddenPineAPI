53617b2fbedc134b45d0e723b49ef69aeade1bfe
chs4-hw <56627743+chs4-hw@users.noreply.github.com>
2020-04-17 10:44:43 +0100

    Update settings.py
    Removed Security Key

--- suddenpine/settings.py
+++ suddenpine/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'xx_33qbfd(0^d7!_ulk!fb57)9xf54be8aln8-r1r40&z80em8'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
